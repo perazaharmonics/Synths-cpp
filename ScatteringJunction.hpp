01053f1534f2e7d22f790e64c33d9936b3027987
J. Enrique Peraza, BScEE MEngEE <141517235+perazaharmonics@users.noreply.github.com>
2025-08-06 06:34:16 -0400

    Best I could do from memory
    Module got broken when I switched the structure and wave propagation method of the waveguides. Tried applying new preparation, priming and wave propagation routines from memory best as I could using my phone.

--- ScatteringJunction.hpp
+++ ScatteringJunction.hpp
@@ -12,7 +12,7 @@
  * * JEP J. Enrique Peraza
  * *
  */
-#pragma once
+pragma once
 #include <cstddef>
 #include <array>
 #include "WGTypes.hpp"
@@ -21,7 +21,7 @@
 #include "BiQuad.hpp"
 namespace sig::wg
 {
-  template<size_t N>
+  template<typename T=float,size_t N,size_t D, T mut, T muf,size_t order=5>
   struct ScatteringJunction final:public Node
   {
     static_assert(N>=2&&N<=8,"juction size unreasonable.");
@@ -31,12 +31,24 @@ namespace sig::wg
     : bra(branches) {}
     
     // No-op Prepare. Branch length and delays should be set externally.
-    bool Prepare(double fs,double /*f0*/,double fc) noexcept
+    bool Prepare(double fs,double /*f0*/,double fc,size_t idelay, double m0, double m1)) noexcept
     {
       this->fs=fs; // Set the sampling frequency.
       this->fc=fc; // Set the cutoff frequency for the damping filter.
+      mut=m0;       // Set Thrian Fractional delay
+      muf=m1;       // Set Farrow fractional delay
       damp=ff.Bessel(fs,fc,0.7071f); // Create the damping filter.
       damp.Clear();  // Clear filter state so it doesn’t attenuate the signal.
+      for (size_t i=0;i<N;++i) // For al branches
+      {                        // configure fractional delays.
+        bra[i].Prepare(idelay,mut,muf);// Prepare the waveguide
+        bra[i].SetFractionalDelay(mut);// Thiran's fractional delay
+        bra[i].SetMuFarrow(muf);// Farrow's Fractional Delay
+      }                         // Done setting up the Fractional delays.
+      // Prime waveguides according to Group Delay
+      size_t maxlat=idelay+static_cast<size_t>(muf+mut);
+      for (size_t i=0;i<maxlat;++i) // For the max group delay
+        Propagate(T(0)); // Circulate an impulse
       return true; // Return true to indicate successful preparation.
     } 
     
@@ -56,7 +68,7 @@ namespace sig::wg
         float sum=0.0f;                 // Sum of incident pressure waves.
         for (size_t i=0;i<N;++i)         // For each branch...
         {
-          inc[i]=bra[i]->Peek();          // Read the incident pressure wave.
+          inc[i]=bra[i]->Read();          // Use Read instead of Peek to read the incident pressure wave.
           sum+=inc[i];                   // Sum the incident pressure waves.
         }                                // Done reading incident pressure waves.
         // ---------------------------- //
@@ -70,7 +82,7 @@ namespace sig::wg
         {
           float out=rho*2.f-inc[i];
           out=damp.ProcessSample(out); // Apply damping to the output.
-          bra[i]->Write(out);             // Write the scattered pressure wave to the branch.
+          bra[i]->Write(out);             // Use Write instead of WriteFrac to write the scattered pressure wave to the branch.
         }                                // Done writing scattered pressure waves.
         // Advance each branch by n samples again.
         for (size_t i=0;i<N;++i)
@@ -80,6 +92,10 @@ namespace sig::wg
     std::array<DelayBranch<>*,N> bra;      // The branches connected to the junction.
     FilterFactory<float> ff; // Filter factory for creating filters.
     BiQuad<float> damp; // Damping filter for the junction.
+    double mut{0.25}; // Fractional delay interpolation factor for Thiran
+    double muf{0.48}; // Fractional delay for Farrow
+    size_t idelay{5}; // The integer delay
+    size_t o{order};  // Order of interplation filters
     double fs{48000.0}; // Sampling frequency.
     double fc{22050.0}; // Cutoff frequency for the damping filter.
   }; // end of ScatteringJunction
